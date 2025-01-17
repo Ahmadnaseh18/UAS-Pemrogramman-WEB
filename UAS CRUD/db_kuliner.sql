8de553a15dbe92d2b8735f8f6cf6a8cd687fa9d6
Ahmadnaseh18 <ahmadnaseh1808@gmail.com>
2025-01-17 12:29:49 +0700

    Add files via upload

---
+++ UAS CRUD/db_kuliner.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 16, 2025 at 08:42 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kuliner`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_makanan`
+--
+
+CREATE TABLE `tbl_makanan` (
+  `id_makanan` int(2) NOT NULL,
+  `nama_makanan` varchar(100) NOT NULL,
+  `daerah_makanan` varchar(100) NOT NULL,
+  `foto_makanan` blob DEFAULT NULL,
+  `keterangan` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_makanan`
+--
+
+INSERT INTO `tbl_makanan` (`id_makanan`, `nama_makanan`, `daerah_makanan`, `foto_makanan`, `keterangan`) VALUES
+(1, 'Nasi Grombyang', 'Pemalang', NULL, NULL),
+(2, 'Tahu ', 'Sumedang', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_minuman`
+--
+
+CREATE TABLE `tbl_minuman` (
+  `id_minuman` int(2) NOT NULL,
+  `nama_minuman` varchar(100) NOT NULL,
+  `daerah_minuman` varchar(100) NOT NULL,
+  `foto_minuman` blob DEFAULT NULL,
+  `keterangan` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_minuman`
+--
+
+INSERT INTO `tbl_minuman` (`id_minuman`, `nama_minuman`, `daerah_minuman`, `foto_minuman`, `keterangan`) VALUES
+(1, 'Es Dawet gula merah', 'Pemalang', NULL, NULL),
+(2, 'Wedang Uwuh', 'Yogyakarta', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_makanan`
+--
+ALTER TABLE `tbl_makanan`
+  ADD PRIMARY KEY (`id_makanan`);
+
+--
+-- Indexes for table `tbl_minuman`
+--
+ALTER TABLE `tbl_minuman`
+  ADD PRIMARY KEY (`id_minuman`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_makanan`
+--
+ALTER TABLE `tbl_makanan`
+  MODIFY `id_makanan` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_minuman`
+--
+ALTER TABLE `tbl_minuman`
+  MODIFY `id_minuman` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
